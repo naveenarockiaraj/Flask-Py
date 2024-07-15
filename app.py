3c7185a2da45d56527babef89403e59ea221c22e
Naveen Arockia Raj <naveen.arockiaraj@view.com>
2024-07-15 17:09:17 +0530

    Get and post api creation

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-store = [
-    {
-        "name":"New store",
-        "item":[
-            {
-                "name":"sofa",
-                "price":25.5
-            }
-        ]
-    }
+stores = [
+    # {
+    #     "name":"New store",
+    #     "item":[
+    #         {
+    #             "name":"sofa",
+    #             "price":25.5
+    #         }
+    #     ]
+    # }
 ]
 
 @app.get("/store")
-def get_store():
-    return {"stores":store}
+def get_stores():
+    return {"stores":stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
