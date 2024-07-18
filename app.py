bb1afd47fa73b97a577694b2f0f3d30137e25694
Naveen Arockia Raj <naveen.arockiaraj@view.com>
2024-07-18 17:31:38 +0530

    data module and error handling

--- app.py
+++ app.py
@@ -1,50 +1,75 @@
+import uuid
 from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    # {
-    #     "name":"New store",
-    #     "item":[
-    #         {
-    #             "name":"sofa",
-    #             "price":25.5
-    #         }
-    #     ]
-    # }
-]
 
 @app.get("/store")
 def get_stores():
-    return {"stores":stores}
+    return {"stores":list(stores.values())}
 
+#stores
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items":[]}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Please include a name for the store."
+        )
+        
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store is already exist")
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}  #**store_data that unpack the values
+    stores[store_id] = store
+    return store, 201
 
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store not found"}, 404
+#items
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Ensure price, store_id, name fields are not empty."
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"item already exists")
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+    if item_data["store_id"] not in stores:
+        return abort(404, message="store not found")
+    
+    item_id = uuid.uuid4().hex
+    item ={**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+@app.get("/item")
+def get_all_item():
+    return {"items": list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return abort(404, message="store not found")
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return abort(404, message="item not found")
